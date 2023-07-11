823a34c005eae6976d28837779689cdcf52b7c74
juliagsy <67888047+juliagsy@users.noreply.github.com>
2023-07-11 14:51:46 +0800

    clean up workflow script and docs directory

--- docs/partial_conf.py
+++
@@ -1,8 +0,0 @@
-# Retrieve html_theme_options from docs/conf.py
-from docs.conf import html_theme_options
-
-html_theme_options["switcher"][
-    "json_url"
-] = "https://lets-unify.ai/docs/versions/ecosystem.json"
-
-repo_name = "ecosystem"
